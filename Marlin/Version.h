3512f752705171cf051eba04ac2bd42b55438859
Autonumous <autonumous@icloud.com>
2017-10-22 19:27:02 +0100

    update version string

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -41,14 +41,14 @@
    * Verbose version identifier which should contain a reference to the location
    * from where the binary was downloaded or the source code was compiled.
    */
-  #define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION " BigBox Dual Hybrid Titan - 20171007"
+  #define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION " BigBox Dual Hybrid Titan - 20171022"
 
   /**
    * The STRING_DISTRIBUTION_DATE represents when the binary file was built,
    * here we define this default string as the date where the latest release
    * version was tagged.
    */
-  #define STRING_DISTRIBUTION_DATE "2017-10-16 23:20"
+  #define STRING_DISTRIBUTION_DATE "2017-10-22 19:20"
 
   /**
    * Required minimum Configuration.h and Configuration_adv.h file versions.
