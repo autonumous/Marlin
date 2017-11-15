1ea4664aa36e566c16cc1e11d5c47f1a8dafbedc
Autonumous <autonumous@icloud.com>
2017-11-15 23:18:09 +0000

    bughunting2

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -9320,8 +9320,9 @@ inline void gcode_M226() {
     #if ENABLED(BABYSTEP_XY)
       for (uint8_t a = X_AXIS; a <= Z_AXIS; a++)
         if (parser.seenval(axis_codes[a]) || (a == Z_AXIS && parser.seenval('S'))) {
-          //const
-          float offs = constrain(parser.value_axis_units(a), -2, 2);
+          //const float offs = constrain(parser.value_axis_units(a), -2, 2);
+          float offs = parser.value_axis_units(a);
+          constrain(offs, -2, 2);
           #if ENABLED(BABYSTEP_ZPROBE_OFFSET)
             if (a == Z_AXIS) {
               zprobe_zoffset += offs;
@@ -9332,8 +9333,9 @@ inline void gcode_M226() {
         }
     #else
       if (parser.seenval('Z') || parser.seenval('S')) {
-        //const
-        float offs = constrain(parser.value_axis_units(Z_AXIS), -2, 2);
+        //const float offs = constrain(parser.value_axis_units(Z_AXIS), -2, 2);
+        float offs = parser.value_axis_units(Z_AXIS);
+        constrain(offs, -2, 2);
         #if ENABLED(BABYSTEP_ZPROBE_OFFSET)
           zprobe_zoffset += offs;
           refresh_zprobe_zoffset(); // This will babystep the axis
