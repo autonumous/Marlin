969b9adbc3aace2533fbe8b6edf50072f0492e02
Autonumous <autonumous@icloud.com>
2018-04-07 02:23:12 +0100

    bugfix

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -11815,7 +11815,7 @@ inline void gcode_T(const uint8_t tmp_extruder) {
       #if ENABLED(BB_CUSTOM_TOOL_CHANGE_BEHAVIOUR_NOMOVE)
        false //(tmp_extruder == active_extruder) || !parser.boolval('S')
       #else
-       //(tmp_extruder == active_extruder) || parser.boolval('S')
+       (tmp_extruder == active_extruder) || parser.boolval('S')
       #endif  //BB_CUSTOM_TOOL_CHANGE_BEHAVIOUR_NOMOVE
     );
 
